6fc52a1d3f599be8210eac268b93ee7e8bd12bd8
JYFFF96 <59434545+JYFFF96@users.noreply.github.com>
2026-09-10 18:03:35 +0800

    Enable English UI on Fairy32 branch

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "laserradar.h"
+#include "englishtranslator.h"
 
 #include <QApplication>
 #include <QFile>
 #include <QStyleFactory>
-#include <QTranslator>
-#include <QLibraryInfo>
+
 void loadStyleSheet(QApplication &app) {
-    QFile file(":./qss/style.qss");  // 直接从磁盘加载
+    QFile file(":./qss/style.qss");
     if (file.open(QFile::ReadOnly)) {
         QString styleSheet = file.readAll();
         app.setStyleSheet(styleSheet);
@@ -15,25 +15,27 @@ void loadStyleSheet(QApplication &app) {
         qDebug() << "Failed to load QSS file!";
     }
 }
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QApplication::setStyle(QStyleFactory::create("Fusion"));
+
     QPalette palette;
-    palette.setColor(QPalette::WindowText, Qt::white); // 设置窗口文本颜色
-    palette.setColor(QPalette::Text, Qt::white);       // 设置文本输入框的文本颜色
-    palette.setColor(QPalette::ButtonText, Qt::white); // 设置按钮文本颜色
+    palette.setColor(QPalette::WindowText, Qt::white);
+    palette.setColor(QPalette::Text, Qt::white);
+    palette.setColor(QPalette::ButtonText, Qt::white);
     qApp->setPalette(palette);
-    // 加载 QSS
+
     loadStyleSheet(a);
-    QTranslator qtTrans;
-#if QT_VERSION >= QT_VERSION_CHECK(6,0,0)
-    qtTrans.load("qtbase_zh_CN", QLibraryInfo::path(QLibraryInfo::TranslationsPath));
-#else  // Qt5
-    qtTrans.load("qtbase_zh_CN", QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-#endif
-    a.installTranslator(&qtTrans);
+
+    // English branch: translate all Qt Designer / tr() strings to English.
+    EnglishTranslator englishTranslator;
+    a.installTranslator(&englishTranslator);
+
     LaserRadar w;
+    // Override the legacy hard-coded Chinese title in LaserRadar's constructor.
+    w.setWindowTitle("LiDAR Debugging Software");
     w.show();
     return a.exec();
 }
